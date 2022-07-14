31e552349e9e43da39dc32061c7e286c79083112
bernhardta <97689985+bernhardta@users.noreply.github.com>
2022-07-14 15:34:46 -0400

    Add files via upload

---
+++ blog/blog.js
@@ -0,0 +1,6 @@
+mybutton = document.getElementById("toTop");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
